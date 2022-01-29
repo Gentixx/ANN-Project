53dbc99e270c231bd7dc5432455de1d4cb56aac1
theCCB <30960801+CemBelentepe@users.noreply.github.com>
2022-01-30 00:32:14 +0300

    UI completed

--- food_pred.py
+++ food_pred.py
@@ -4,18 +4,21 @@ from random import randint
 
 class Predictor:
     def __init__(self,data,use_predetermined=True):
-        self.model=SVC()
-        self.data=data
-        self.use_predetermined=use_predetermined
-    def init_train(self, X,y):
+        self.model = SVC()
+        self.data = data
+        self.use_predetermined = use_predetermined
+    
+    def init_train(self, X, y):
         pass
-    def update(self,X,y):
+    
+    def update(self, X, y):
         pass
-    def get_best_random(self,n,threshold):
-        return randint(0, 279215)
-    def get_random(self,num):
-        seed(1)
-        return[randint(0, 279215) for i in range(num)]
+    
+    def get_best_random(self, n=512, threshold=0.8):
+        return randint(0, len(self.data.index)-1), 0.8
+    
+    def get_random(self, num=1):
+        return [randint(0, len(self.data.index)-1) for i in range(num)]
+    
     def __new_params(self):
         pass
-#df.iloc[0]["additives"]
--- main.py
+++ main.py
@@ -1,64 +1,9 @@
-import pandas as pd
-import numpy as np
-
-input_len = 128
-
-def read_data(filename):
-    df=pd.read_csv(filename, sep="\t",
-                   usecols=["product_name","brands","additives"])
-    df=df.dropna()
-    print("[INFO] Data loaded")
-    return df
+from food_ui import *
 
 
-def parse_additives(expr):
-    l_expr = [x.split("->")[0].strip() for x in expr.split('[') if "en:" in x]
-    l_expr = [x for x in l_expr if x]
-    return l_expr
-    
-
-def clear_data(df):
-    df["length"] = df["parsed_additives"].apply(lambda x: len(x))
-    df = df[df["length"] > 0]    
-    print("[INFO] Data Parsed")
-
-    hist = {}
-    def add_to_hist(el):
-        for x in el:
-            if x in hist:
-                hist[x] += 1
-            else:
-                hist[x] = 1
-    
-    df["parsed_additives"].apply(add_to_hist)
-    print("[INFO] Histogram Created")
-    hist = sorted(hist.items(), key=lambda item: item[1], reverse=True)
-    hist = np.array(hist[:input_len])
-    names = hist[:, 0]
-    hist = hist[:,1].astype(np.uint32)
-    print("[INFO] Histogram Sorted")
-    
-    def additive_to_vec(additives):
-        vec = np.zeros(input_len)
-        for x in additives:
-            if x in names:
-                vec[np.where(names==x)] = 1
-        return vec
-    
-    df["additives"] = df["parsed_additives"].apply(additive_to_vec)
-    print("[INFO] Additive Vector Created")
-    
-    df = df.reset_index()
-    df = df.drop(columns=["index", "parsed_additives", "length"])
-    print("[INFO] Dataframe is created")
-    return df
-
-
-def main():
-    global df
-    df = read_data("data.tsv")
-    df["parsed_additives"] = df["additives"].apply(parse_additives)
-    df = clear_data(df)
+def main():    
+    app = FoodPredictor()
+    app.run()    
 
 
 if __name__ == "__main__":
